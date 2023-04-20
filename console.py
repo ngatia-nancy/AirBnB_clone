189d89cb22079f15d7ffcea288a8b8f3c52e39e9
ngatia-nancy <nancyngatia1@gmail.com>
2023-04-20 12:53:51 -0700

    console

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """AirBnB command Interpreter"""
+
+    prompt= "(hbnb)"
+    
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+    
+    def do_EOF(self, arg):
+        """Signal to exit the program"""
+        return True
+    
+    def emptyline(self):
+        """Execute nothing upon receiving empty line + Enter"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
